2321b61c8be364d6e54db74f8894b1d4fe27ce82
Dan Thayer <dan@chi.swan.ac.uk>
2020-04-29 15:24:09 +0100

    Added an example print statement during demo.

--- hello_world.R
+++ hello_world.R
@@ -2,3 +2,4 @@ print("This is a trivial R project!");
 print("Isn't this exciting? Neat!");
 print("Hello, world!");
 print("The possibilities are endless!");
+print("This is another print statement. Yay!");
